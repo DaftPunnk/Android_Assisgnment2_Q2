ca8df0b8ddc1f21bab9c85d995c860dc710915cf
DaftPunnk <chenzhaokun@vip.qq.com>
2020-05-05 21:28:08 +1200

    No Error on IDE but keep crashing on emulator

---
+++ app/src/main/java/com/example/assisgnment2_q2/Utils.java
@@ -0,0 +1,7 @@
+package com.example.assisgnment2_q2;
+
+public class Utils {
+
+    public static final String EMAIL = "bayernghost@gmail.com";
+    public static final String PASSWORD = "137280czk";
+}
